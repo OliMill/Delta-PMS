4425d647bcd3e18d88c771e36222f58683699efb
OliMill <4-omillard@bayhouse.gfmat.org>
2026-03-11 15:06:12 +0000

    Update Customer.java

--- src/com/hotelmanagement/models/Customer.java
+++ src/com/hotelmanagement/models/Customer.java
@@ -2,72 +2,20 @@ package com.hotelmanagement.models;
 
 import java.time.LocalDate;
 
-public class Customer {
-    private int customerID;
-    private String firstName;
-    private String lastName;
-    private LocalDate dob;
-    private String email;
-    private String passwordHash;
+public class Customer extends User {
 
-    //customer model
     public Customer(int customerID, String firstName, String lastName, LocalDate dob,
-    String email,String passwordHash) {
-        this.customerID = customerID;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dob = dob;
-        this.email = email;
-        this.passwordHash = passwordHash;
+                    String email, String passwordHash) {
+        // Pass everything up to the User constructor
+        super(customerID, firstName, lastName, dob, email, passwordHash);
     }
 
-    // getters
+    // Wrappers to maintain backward compatibility
     public int getCustomerID() {
-        return customerID;
+        return super.getId();
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public LocalDate getDob() {
-        return dob;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPasswordHash() {
-        return passwordHash;
-    }
-    
-    // setters
     public void setCustomerID(int customerID) {
-        this.customerID = customerID;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
+        super.setId(customerID);
     }
 }
